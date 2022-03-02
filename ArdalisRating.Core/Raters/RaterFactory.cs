50ef3dc0ce61ffece00b11eb6b2f0076b69b4c3a
Sergey Pauls <paulssergey@gmail.com>
2022-03-02 17:59:46 +0300

    Create raters and factory of raters
    We expect that our portfolio of raters is going to continue to grow.
    Rating Engine will not be changed when new rater will be added.

---
+++ ArdalisRating.Core/Raters/RaterFactory.cs
@@ -0,0 +1,25 @@
+﻿using ArdalisRating.Domain;
+using System;
+
+namespace ArdalisRating.Core.Raters
+{
+    public class RaterFactory
+    {
+        public RaterBase Create(Policy policy, RatingEngine engine)
+        {
+            switch (policy.Type)
+            {
+                case PolicyType.Life:
+                    return new LifePolicyRater(engine, engine.Logger);
+                case PolicyType.Land:
+                    return new LandPolicyRater(engine, engine.Logger);
+                case PolicyType.Auto:
+                    return new AutoPolicyRater(engine, engine.Logger);
+                default:
+                    throw new NotImplementedException();
+                    // TODO: Implement Null Object Pattern
+                    // Logger.Log("Unknown policy type")
+            }
+        }
+    }
+}
